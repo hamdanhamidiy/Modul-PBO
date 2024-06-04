b3b37beb0dce4f510e6624b3262a72da11643fba
hamdanhamidiy <127773344+hamdanhamidiy@users.noreply.github.com>
2024-06-04 19:32:53 +0700

    Add files via upload

---
+++ modultugas6/exception/custom/IllegalAdminAccess.java
@@ -0,0 +1,7 @@
+package org.example.TUGAS6MDL6.exception.custom;
+
+public class IllegalAdminAccess extends Exception {
+    public IllegalAdminAccess(String message) {
+        super(message);
+    }
+}
